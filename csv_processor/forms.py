704b48166bc01459a1587178c0d54285fccd7261
Fabricio David Galarza Piedrahita <fabricio.galarza@olsoft.com.co>
2025-04-03 16:48:33 -0500

    Actualizaciones de funcionalidades

--- csv_processor/forms.py
+++ csv_processor/forms.py
@@ -2,13 +2,30 @@ from django import forms
 
 # Opciones de formato para el archivo CSV (estructuras diferentes)
 FILE_CHOICES = [
+    ('0', 'Seleccionar'),
     ('1005', 'Cargar Fomato 1005'),
     ('1006', 'Cargar Fomato 1006'),
     ('1007', 'Cargar Fomato 1007'),
 ]
 
-class CSVUploadFrom(forms.Form):
+EXCEL_CHOISES = [
+    ('0', 'Seleccionar'),
+    ('1', 'Proveedores'),
+    ('2', 'Archivo 5007'),
+]
+
+class CSVUploadForm(forms.Form):
     #Campo para seleccionar el tipo de formato
-    file_format = forms.ChoiceField(choices=FILE_CHOICES, label="Selecciona el Formato")
+    file_format = forms.ChoiceField(
+        choices=FILE_CHOICES, label="Selecciona el Formato",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
     #Campo para cargar el archvo CSV
-    csv_file = forms.FileField(label="")
+    csv_file = forms.FileField(label="Archivo")
+
+# form para proveedores
+class ExcelUploadFrom(forms.Form):
+    #Campo para seleccionar el tipo de formato
+    excel_file_proveedor = forms.FileField(label="Proveedores")
+
+
