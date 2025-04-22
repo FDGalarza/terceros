d76dde36e353051a9e94fad338bf896422a5a041
Fabricio David Galarza Piedrahita <fabricio.galarza@olsoft.com.co>
2025-04-22 16:07:52 -0500

    cambio en vercel.json

--- procesar_csv/settings.py
+++ procesar_csv/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
