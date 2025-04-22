ff6bc062f2a585bd4e148218afc3811dfff0cfc3
Fabricio David Galarza Piedrahita <fabricio.galarza@olsoft.com.co>
2025-04-22 09:24:55 -0500

    Correccion setings para solución error de archivos staticos

--- procesar_csv/settings.py
+++ procesar_csv/settings.py
@@ -123,7 +123,11 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
