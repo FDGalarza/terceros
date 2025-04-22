fe0df5826e0f9817b13f7d6b390e7a4c6098daf0
Fabricio David Galarza Piedrahita <fabricio.galarza@olsoft.com.co>
2025-04-22 16:53:03 -0500

    Se redefine vercel.json

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -1,6 +1,12 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'procesar_csv.settings')
+# Establecer el entorno de configuración de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "procesar_csv.settings")
 
-app = get_wsgi_application()
+# Ejecutar collectstatic automáticamente al inicio
+call_command('collectstatic', '--noinput')
+
+application = get_wsgi_application()
