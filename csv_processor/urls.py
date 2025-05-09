cf0ce1ca671a2de9c8594ddfdbea7ff480dd708d
Fabricio David Galarza Piedrahita <fabricio.galarza@olsoft.com.co>
2025-05-09 09:15:38 -0500

    Elimina ruta porocesar CSV

--- csv_processor/urls.py
+++ csv_processor/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('procesar_csv/', views.procesar_csv, name='procesar_csv'),
     path('procesar_excel/', views.procesar_excel, name='procesar_excel'),
     path('proveedores/', views.proveedores, name='procesar_proveedores'),
     path('crear_tarea/', views.crear_tarea, name='crear_tarea'),
